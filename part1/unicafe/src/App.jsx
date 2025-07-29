dc4162da8473ca926fd589c56278d3e58582aa37
poliveir <pedrol2507@gmail.com>
2025-07-29 19:00:34 +0100

    1.11*: unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,7 +2,15 @@ import { useState } from 'react'
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>;
 
-const StatisticLine = ({text, value}) => <p>{text.before} {value} {text.after}</p>;
+const StatisticLine = ({text, value}) => {
+	return (
+		<tr>
+			<td>{text.before}</td>
+			<td>{value}</td>
+			<td>{text.after}</td>
+		</tr>
+	);
+}
 
 const Statistics = ({statistics}) => {
 	const sum = Object.values(statistics).reduce((acc, curr) => acc + curr, 0);
@@ -25,53 +33,57 @@ const Statistics = ({statistics}) => {
 		<>
 			<h1>statistics</h1>
 
-			<StatisticLine
-				text={{
-					before: "good",
-					after: null
-				}}
-				value={statistics.good}
-			></StatisticLine>
+			<table>
+				<tbody>
+					<StatisticLine
+						text={{
+							before: "good",
+							after: null
+						}}
+						value={statistics.good}
+					></StatisticLine>
 
-			<StatisticLine
-				text={{
-					before: "neutral",
-					after: null
-				}}
-				value={statistics.neutral}
-			></StatisticLine>
+					<StatisticLine
+						text={{
+							before: "neutral",
+							after: null
+						}}
+						value={statistics.neutral}
+					></StatisticLine>
 
-			<StatisticLine
-				text={{
-					before: "bad",
-					after: null
-				}}
-				value={statistics.bad}
-			></StatisticLine>
+					<StatisticLine
+						text={{
+							before: "bad",
+							after: null
+						}}
+						value={statistics.bad}
+					></StatisticLine>
 
-			<StatisticLine
-				text={{
-					before: "all",
-					after: null
-				}}
-				value={sum}
-			></StatisticLine>
+					<StatisticLine
+						text={{
+							before: "all",
+							after: null
+						}}
+						value={sum}
+					></StatisticLine>
 
-			<StatisticLine
-				text={{
-					before: "average",
-					after: null
-				}}
-				value={average}
-			></StatisticLine>
+					<StatisticLine
+						text={{
+							before: "average",
+							after: null
+						}}
+						value={average}
+					></StatisticLine>
 
-			<StatisticLine
-				text={{
-					before: "positive",
-					after: "%"
-				}}
-				value={positive}
-			></StatisticLine>
+					<StatisticLine
+						text={{
+							before: "positive",
+							after: "%"
+						}}
+						value={positive}
+					></StatisticLine>
+				</tbody>
+			</table>
 		</>
 	);
 
